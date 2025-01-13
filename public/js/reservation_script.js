c9b00b319e7b3aaeae69549ab9fac6421eead05b
Denis <denis.mladenek01@gmail.com>
2025-01-13 12:23:46 +0100

    Login

--- public/js/reservation_script.js
+++ public/js/reservation_script.js
@@ -3,6 +3,23 @@ document.getElementById('button').addEventListener('click', function (event) {
     const time = document.getElementById('time').value;
     const date = document.getElementById('date').value;
 
+    const parts = time.split('.');
 
+    let string1 = '';
+    let string2 = '';
+
+    if (parts.length !== 2) {
+        string1 = 'Zle zadaný čas (príklad: 15.5 => 15:30 | 12.0 => 12:00)';
+    }
+
+    if (date.length != 8) {
+        string2 = 'Zle zadaný dátum (príklad: 20250110 => 01.10.2025)'
+    }
+
+    if (string2 != '' || string1 != '') {
+        let string = string1 + '\n' + string2;
+        alert(string);
+        event.preventDefault();
+    }
 
 })
